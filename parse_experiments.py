0c85cf7eaec8d9f92a76cf09afcc1843a3d83658
ai21m027 <ai21m027@technikum-wien.at>
2023-04-03 18:34:45 +0200

    parse_experiments.py can now handle eval files

--- parse_experiments.py
+++ parse_experiments.py
@@ -22,25 +22,33 @@ class Experiment(NamedTuple):
     fp: int
     tp: int
 
-def parse_log(path: str,dir:str) -> Experiment:
+def parse_log(path: str,dir:str,is_eval=False) -> Experiment:
     params = dir.split('_')
     #hidden_size = int(params[0])
     #layers = int(params[1])
-    with open(path, 'r') as f:
-        lines = f.readlines()
+    train_log=os.path.join(os.path.split(path)[0],'train.log')
+    if is_eval:
+        with open(train_log, 'r') as f:
+            lines = f.readlines()
+    else:
+        with open(path, 'r') as f:
+            lines = f.readlines()
     found = False
     lr = float(re.search("'lr': (\d.\d+)",lines[0]).group(1))
     seed = int(re.search("'seed': (\d+)",lines[0]).group(1))
-    subtitle_type = re.search("'subtitletype': '(\w+)'",lines[0]).group(1)
+    subtitle_type = re.search("'subtitletype': '(\w+)'",lines[0]).group(1) if re.search("'subtitletype': '(\w+)'",lines[0]) is not None else 'manual'
     epoch = int(re.search("'epochs': (\d+)",lines[0]).group(1))
     hidden = int(re.search("'hidden_size': (\d+)",lines[0]).group(1))
     num_layers = int(re.search("'num_layers': (\d+)",lines[0]).group(1))
     type = re.search("'type': '(\w+)'", lines[0]).group(1) if re.search("'type': '(\w+)'",lines[0]) is not None else 'classification'
+    if is_eval:
+        with open(path, 'r') as f:
+            lines = f.readlines()
     for idx,line in enumerate(lines):
         if re.search(r'Validating Epoch', line):
             accuracy = float(re.search(r'accuracy: (\d+\.\d+),', line).group(1))
             pk = float(re.search(r'Pk: (\d+\.\d+),', line).group(1))
-            windiff = float(re.search(r'Windiff: (\d+\.\d+),', line).group(1))
+            windiff = float(re.search(r'Windiff: (-?\d+\.?\d*),', line).group(1))
             f1 = float(re.search(r'F1: (\d+\.\d+) ,', line).group(1))
             loss = float(re.search(r'Loss: (\d+\.\d+)', line).group(1))
             found = True
@@ -73,16 +81,16 @@ def parse_log(path: str,dir:str) -> Experiment:
         return None
 
 if __name__ == '__main__':
-    experiment_base_dir = r'C:\temp\target_type_10000'
+    experiment_base_dir = r'C:\temp\ModelSize_2nd_finished'
     pd.set_option('display.max_columns',None)
     experiments = []
     for root, dirs, files in walk(experiment_base_dir):
         for file in files:
-            if file.endswith('train.log'):
+            if file.endswith('eval.log'):
                 #print(dir,file)
                 test = root
                 if root.split('\\')[-1] != r'checkpoints':
-                    experiments.append(parse_log(f'{root}\\{file}',root.split('\\')[-1]))
+                    experiments.append(parse_log(f'{root}\\{file}',root.split('\\')[-1],is_eval=True))
 
     experiments_df = pd.DataFrame(experiments)
     #experiments_df.columns = ['epoch','hidden','layers','seed','Acc','Pk','Windiff','F1','Loss','TN','FN','FP','TP']
